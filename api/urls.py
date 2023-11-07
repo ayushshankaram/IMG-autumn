4937e4ce2fc7e4cd06b98cd8cab54ceb88a7fda5
ayushshankaram <ayushshankaram@gmail.com>
2023-11-07 22:17:30 +0530

    first commit with Oauth

---
+++ api/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('auth/authorize/', views.OAuthAuthorizeView.as_view(), name='oauth-authorize'),
+    path('auth/callback/', views.oauth2_callback.as_view(), name='oauth-callback'),
+    # path('api/users/', views.userviews.UserList.as_view(),),
+    # path('api/user/<int:user_id>/', views.userviews.UserDetailView.as_view(), name='user-detail'),
+    path('get_user_data/', views.GetUserDataView.as_view(), name='get_user_data'),
+
+]
